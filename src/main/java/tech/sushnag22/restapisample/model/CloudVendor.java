b63d485922b038d195ab69f8ee6a9c99e51dbbf3
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-02 11:55:37 +0530

    Add getters and setters to model CloudVendor.java class

--- src/main/java/tech/sushnag22/restapisample/model/CloudVendor.java
+++ src/main/java/tech/sushnag22/restapisample/model/CloudVendor.java
@@ -15,4 +15,28 @@ public class CloudVendor {
         this.vendorAddress = vendorAddress;
     }
 
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(String vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getVendorAddress() {
+        return vendorAddress;
+    }
+
+    public void setVendorAddress(String vendorAddress) {
+        this.vendorAddress = vendorAddress;
+    }
+
 }
