68ce6ada44bb8ccce8f545b9ae1b512b069356de
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-02 13:28:54 +0530

    Update read method of controller CloudVendorApiService.java class

--- src/main/java/tech/sushnag22/restapisample/controller/CloudVendorApiService.java
+++ src/main/java/tech/sushnag22/restapisample/controller/CloudVendorApiService.java
@@ -1,18 +1,19 @@
 package tech.sushnag22.restapisample.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.sushnag22.restapisample.model.CloudVendor;
 
 @RestController
 @RequestMapping("/cloud-vendors")
 public class CloudVendorApiService {
 
+    CloudVendor cloudVendor;
+
     @GetMapping("{vendorId}")
-    public CloudVendor getCloudVendorDetails(String vendorId) {
-        return new CloudVendor("1", "Amazon",
-                "United States of America");
+    public CloudVendor readCloudVendorDetails(String vendorId) {
+        return cloudVendor;
+//                new CloudVendor("1", "Amazon",
+//                "United States of America");
     }
 
 }
