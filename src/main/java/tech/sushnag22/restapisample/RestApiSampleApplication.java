5cd4c92c2030ed3f96d7326bc4885f10c5b820b4
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-02 11:31:18 +0530

    Initial commit

---
+++ src/main/java/tech/sushnag22/restapisample/RestApiSampleApplication.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.restapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiSampleApplication.class, args);
+    }
+
+}
